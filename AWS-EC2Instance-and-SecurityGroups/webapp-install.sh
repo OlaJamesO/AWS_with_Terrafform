8c85159e1288c6726fd2327344a2a781f1014f85
jokuso01 <okusolubo@gmail.com>
2022-10-18 03:45:57 +0100

    created vpc hosted a private-web server with bastion

---
+++ AWS-EC2Instance-and-SecurityGroups/webapp-install.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+# Instance Identity Metadata Reference - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
+
+# Installing Dependencies
+echo "########################################"
+echo "Installing packages."
+echo "########################################"
+sudo yum install wget unzip httpd -y > /dev/null
+echo
+
+# Start & Enable Service
+echo "########################################"
+echo "Start & Enable HTTPD Service"
+echo "########################################"
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo
+
+# Creating Temp Directory
+echo "########################################"
+echo "Starting Artifact Deployment"
+echo "########################################"
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+echo
+
+wget https://www.tooplate.com/zip-templates/2098_health.zip > /dev/null
+unzip 2098_health.zip > /dev/null
+sudo cp -r 2098_health/* /var/www/html/
+echo
+
+# Bounce Service
+echo "########################################"
+echo "Restarting HTTPD service"
+echo "########################################"
+systemctl restart httpd
+echo
+
+# Clean Up
+echo "########################################"
+echo "Removing Temporary Files"
+echo "########################################"
+rm -rf /tmp/webfiles
+echo
+
+sudo systemctl status httpd
+ls /var/www/html/
+
+sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
